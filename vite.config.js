5ae84364b23f3c1d9f9b78321ccf3917f3b3b4b0
Chelsea <s094334@gmail.com>
2026-06-08 15:47:05 +0800

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/2026-06-08-layout-exam/',
   plugins: [
     tailwindcss(),
   ],
